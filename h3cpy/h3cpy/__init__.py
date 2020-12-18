bb84c597e89a0664f3edf11a4c4ad2d492e52dad
Nico Mandery <nico.mandery@dlr.de>
2020-12-18 07:55:33 +0100

    proof of concepts for numpy and pandas integration

--- h3cpy/h3cpy/__init__.py
+++ h3cpy/h3cpy/__init__.py
@@ -1,8 +1,18 @@
 
-from .h3cpy import CompactedTable, version
+# import from rust library
+from .h3cpy import CompactedTable, version, poc_some_h3indexes
+import pandas as pd
 
 __all__ = [
-    "CompactedTable"
+    "CompactedTable",
+    "poc_some_dataframe"
 ]
 
 __version__ = version()
+
+
+# proof of concepts - to be removed later
+def poc_some_dataframe():
+    return pd.DataFrame({
+        "h3index": poc_some_h3indexes()
+    })
--- h3cpy/src/lib.rs
+++ h3cpy/src/lib.rs
@@ -3,9 +3,11 @@ mod inspect;
 
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use numpy::{PyArray, Ix1, IntoPyArray};
 use crate::{
     inspect::CompactedTable
 };
+use h3::index::Index;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -17,6 +19,8 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pyfunction]
 fn version() -> PyResult<String> { Ok(env!("CARGO_PKG_VERSION").to_string()) }
 
+
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn h3cpy(py: Python, m: &PyModule) -> PyResult<()> {
@@ -24,5 +28,13 @@ fn h3cpy(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(version, m)?)?;
 
+    /// proof-of-concept for numpy integration. using u64 as this will be the type for h3 indexes
+    /// TODO: remove later
+    #[pyfn(m, "poc_some_h3indexes")]
+    fn poc_some_h3indexes(py: Python) -> &PyArray<u64, Ix1> {
+        let idx: Index = 0x89283080ddbffff_u64.into();
+        let v: Vec<_> = idx.k_ring(20).iter().map(|i| i.h3index()).collect();
+        v.into_pyarray(py)
+    }
     Ok(())
 }
