e8bdd3e9e2d9aa207e2ed8bddb532d014c78f7f1
Nico Mandery <nico.mandery@dlr.de>
2020-12-18 10:12:23 +0100

    stub for clickhouseconnection

--- h3cpy/h3cpy/__init__.py
+++ h3cpy/h3cpy/__init__.py
@@ -1,10 +1,11 @@
 
 # import from rust library
-from .h3cpy import CompactedTable, version, poc_some_h3indexes
+from .h3cpy import CompactedTable, ClickhouseConnection, version
 import pandas as pd
 
 __all__ = [
     "CompactedTable",
+    "ClickhouseConnection",
     "poc_some_dataframe"
 ]
 
@@ -14,5 +15,5 @@ __version__ = version()
 # proof of concepts - to be removed later
 def poc_some_dataframe():
     return pd.DataFrame({
-        "h3index": poc_some_h3indexes()
+        "h3index": ClickhouseConnection().poc_some_h3indexes()
     })
--- h3cpy/src/lib.rs
+++ h3cpy/src/lib.rs
@@ -1,40 +1,51 @@
 mod window;
 mod inspect;
 
-use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
+use pyo3::{
+    prelude::*,
+    wrap_pyfunction,
+    Python
+};
 use numpy::{PyArray, Ix1, IntoPyArray};
 use crate::{
     inspect::CompactedTable
 };
 use h3::index::Index;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// version of the module
 #[pyfunction]
 fn version() -> PyResult<String> { Ok(env!("CARGO_PKG_VERSION").to_string()) }
 
+#[pyclass]
+#[derive(Clone)]
+struct ClickhouseConnection {}
+
+#[pymethods]
+impl ClickhouseConnection {
+
+    #[new]
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    /// proof-of-concept for numpy integration. using u64 as this will be the type for h3 indexes
+    /// TODO: remove later
+    pub fn poc_some_h3indexes(&self) -> PyResult<Py<PyArray<u64, Ix1>>> {
+        let idx: Index = 0x89283080ddbffff_u64.into();
+        let v: Vec<_> = idx.k_ring(80).iter().map(|i| i.h3index()).collect();
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+        Ok(v.into_pyarray(py).to_owned())
+    }
+}
 
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn h3cpy(py: Python, m: &PyModule) -> PyResult<()> {
     m.add("CompactedTable", py.get_type::<CompactedTable>())?;
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add("ClickhouseConnection", py.get_type::<ClickhouseConnection>())?;
     m.add_function(wrap_pyfunction!(version, m)?)?;
 
-    /// proof-of-concept for numpy integration. using u64 as this will be the type for h3 indexes
-    /// TODO: remove later
-    #[pyfn(m, "poc_some_h3indexes")]
-    fn poc_some_h3indexes(py: Python) -> &PyArray<u64, Ix1> {
-        let idx: Index = 0x89283080ddbffff_u64.into();
-        let v: Vec<_> = idx.k_ring(80).iter().map(|i| i.h3index()).collect();
-        v.into_pyarray(py)
-    }
     Ok(())
 }
