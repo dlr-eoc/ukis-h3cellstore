0b439939c230152d25058cf407afd28d344ff694
mand_nc <nico.mandery@dlr.de>
2021-01-19 12:22:33 +0100

    bugfix: make the window functionality work with realy small polygons

--- h3cpy/src/window.rs
+++ h3cpy/src/window.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 use geo::algorithm::{
     bounding_rect::BoundingRect,
+    centroid::Centroid,
 };
 use geo_types::{Polygon, Rect};
 use h3ron::{Index, polyfill};
@@ -51,17 +52,28 @@ pub fn create_window(window_polygon: Polygon<f64>, table_set: &TableSet, target_
     let window_res = window_index_resolution(table_set, target_h3_resolution, window_max_size);
 
     let mut window_index_set = HashSet::new();
-    for h3index in polyfill(&window_polygon, window_res) {
-        let index = Index::from(h3index);
-        window_index_set.insert(index);
-
+    let mut add_index = |index: Index| {
         // polyfill just uses the centroid to determinate if an index is convert,
         // but we also want intersecting h3 cells where the centroid may be outside
         // of the polygon, so we add the direct neighbors as well.
-        for ring_h3index in Index::from(h3index).k_ring(1) {
+        for ring_h3index in index.k_ring(1) {
             window_index_set.insert(ring_h3index);
         }
+        window_index_set.insert(index);
+    };
+
+    for h3index in polyfill(&window_polygon, window_res) {
+        let index = Index::from(h3index);
+        add_index(index);
     }
+
+    // for small windows, polyfill may not yield results,
+    // so just adding the center as well.
+    if let Some(point) = window_polygon.centroid() {
+        let index = Index::from_coordinate(&point.0, target_h3_resolution);
+        add_index(index);
+    }
+
 
     Ok(SlidingH3Window {
         window_polygon,
