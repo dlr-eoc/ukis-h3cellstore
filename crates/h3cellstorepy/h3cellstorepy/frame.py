f0ab6db463adc1a5ddbbe1168315cb7630554abf
Nico Mandery <nico.mandery@dlr.de>
2022-04-25 15:08:00 +0200

    accessor for h3index_column_name

--- crates/h3cellstorepy/h3cellstorepy/frame.py
+++ crates/h3cellstorepy/h3cellstorepy/frame.py
@@ -24,20 +24,25 @@ class DataFrameWrapper:
     def __init__(self, df: typing.Union[PyDataFrame, PyH3DataFrame, pa.Table, "pl.DataFrame", "pd.DataFrame"]):
         self._df = df
 
+    def h3index_column_name(self) -> str:
+        """name of the column the h3indexes are stored in"""
+        if isinstance(self._df, PyDataFrame) or isinstance(self._df, PyH3DataFrame):
+            return self._df.h3index_column_name()
+        else:
+            raise ValueError("contained type holds nt h3index_column_name meta information")
+
     def to_arrow(self) -> pa.Table:
         if isinstance(self._df, pa.Table):
             return self._df
         if isinstance(self._df, PyDataFrame) or isinstance(self._df, PyH3DataFrame):
             chunks = self._df.to_arrow()
             return pa.Table.from_batches(chunks)
-
         if _HAS_POLARS:
             if isinstance(self._df, pl.DataFrame):
                 return self._df.to_arrow()
         if _HAS_PANDAS:
             if isinstance(self._df, pd.DataFrame):
                 return pa.Table.from_pandas(self._df)
-
         raise TypeError("unsupported type")
 
     def to_polars(self) -> "pl.DataFrame":
@@ -54,7 +59,7 @@ class DataFrameWrapper:
         raise TypeError("unsupported type")
 
     def to_pandas(self) -> "pd.DataFrame":
-        if not _HAS_POLARS:
+        if not _HAS_PANDAS:
             raise RuntimeError("pandas is required")
         if isinstance(self._df, pd.DataFrame):
             return self._df
